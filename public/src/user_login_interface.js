bef566e393f3a642f75dc961ada2d542efe640f1
Charles Kettell <charleskettell@hotmail.co.uk>
2019-08-01 14:59:24 +0100

    update to the login jquery

--- public/src/user_login_interface.js
+++ public/src/user_login_interface.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
     var userName = $('#username_textbox').val()
     var password = $('#password_textbox').val()
 
-};
-});
     $.post('http://localhost:9292/user/login/attempt', User.login(userName, password),
       function(data){
         if(data=='true'){
